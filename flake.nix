5155537ec75a449c4139af7a07db4da33e9afff7
mitchmindtree <mail@mitchellnordine.com>
2022-08-17 18:36:59 +1000

    Add forc, forc-plugins, fuel-core and fuel-gql-cli pkgs with devShell
    This adds the following packages to the flake:
    
    - forc
    - forc-explore
    - forc-fmt
    - forc-lsp
    - fuel-core
    - fuel-gql-cli
    
    Each package is currently built from a pinned instance of the master
    branch of their respective repo and is not associated with a particular
    version. In the future, the aim is to allow for selecting any git tag
    version.
    
    Currently the latest stable Rust release provided by oxalica's
    rust-overlay is used (1.63 as of writing this). This can be updated if
    necessary with `nix flake lock --update-input rust-overlay`.
    
    A `devShell` has also been added that combines all of the inputs for the
    packages above under a single shell for ease of spinning up a
    development environment.

--- flake.nix
+++ flake.nix
@@ -30,11 +30,11 @@
 
   outputs = inputs: let
     utils.supportedSystems = [
-      # "aarch64-linux"
-      # "i686-linux"
+      "aarch64-linux"
+      "i686-linux"
       "x86_64-linux"
-      # "aarch64-darwin"
-      # "x86_64-darwin"
+      "aarch64-darwin"
+      "x86_64-darwin"
     ];
     utils.eachSupportedSystem =
       inputs.utils.lib.eachSystem utils.supportedSystems;
@@ -44,26 +44,104 @@
         name = "fuel-core";
         version = "master";
         src = inputs.fuel-core-src;
-        cargoSha256 = "sha256-kyMRWIRNxOohrloMrbHP526sxqqyt7zSBXW37y8rhLo=";
+        cargoLock.lockFile = "${inputs.fuel-core-src}/Cargo.lock";
+        buildAndTestSubdir = "fuel-core";
+        nativeBuildInputs = [
+          pkgs.clang
+          pkgs.pkg-config
+          rust-platform.rust.cargo
+          rust-platform.rust.rustc
+        ];
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+        meta = {
+          homepage = "https://github.com/fuellabs/fuel-core";
+          license = pkgs.lib.licenses.bsl11;
+        };
+      };
+
+      fuel-gql-cli = rust-platform.buildRustPackage {
+        name = "fuel-gql-cli";
+        version = "master";
+        src = inputs.fuel-core-src;
+        cargoLock.lockFile = "${inputs.fuel-core-src}/Cargo.lock";
+        buildAndTestSubdir = "fuel-client";
         nativeBuildInputs = [
           rust-platform.rust.cargo
           rust-platform.rust.rustc
-          pkgs.pkg-config
-          pkgs.clang
-          pkgs.libclang
         ];
-        buildInputs = [
-          pkgs.grpc-tools
-          pkgs.openssl
-        ];
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-        PROTOC = "${pkgs.grpc-tools}/bin/protoc";
+        meta = {
+          homepage = "https://github.com/fuellabs/fuel-core";
+          license = pkgs.lib.licenses.bsl11;
+        };
       };
 
-      # forc = {};
-      # forc-explore = {};
-      # forc-fmt = {};
-      # forc-lsp = {};
+      forc = rust-platform.buildRustPackage {
+        name = "forc";
+        version = "master";
+        src = inputs.sway-src;
+        cargoHash = "sha256-Va6MqFiYxMn3NWU0xOKOlGec/Qus4Z3dSSCrUid7cKY=";
+        buildAndTestSubdir = "forc";
+        nativeBuildInputs = [
+          pkgs.perl # for openssl-sys
+          pkgs.pkg-config # for openssl-sys
+          rust-platform.rust.cargo
+          rust-platform.rust.rustc
+        ];
+        meta = {
+          homepage = "https://github.com/fuellabs/sway";
+          license = pkgs.lib.licenses.asl20;
+        };
+      };
+
+      forc-explore = rust-platform.buildRustPackage {
+        name = "forc-explore";
+        version = "master";
+        src = inputs.sway-src;
+        cargoHash = "sha256-3xnrgnEJFEWQ1JS1RwuNKvYP68SFyOPrnBxr++1XL4k=";
+        buildAndTestSubdir = "forc-plugins/forc-explore";
+        nativeBuildInputs = [
+          rust-platform.rust.cargo
+          rust-platform.rust.rustc
+        ];
+        meta = {
+          homepage = "https://github.com/fuellabs/sway";
+          license = pkgs.lib.licenses.asl20;
+        };
+      };
+
+      forc-fmt = rust-platform.buildRustPackage {
+        name = "forc-fmt";
+        version = "master";
+        src = inputs.sway-src;
+        cargoHash = "sha256-nufph2fKJs6IjAH8flxHuh4emCo2o8Kr/T1hh2oW3DI=";
+        buildAndTestSubdir = "forc-plugins/forc-fmt";
+        nativeBuildInputs = [
+          rust-platform.rust.cargo
+          rust-platform.rust.rustc
+        ];
+        meta = {
+          homepage = "https://github.com/fuellabs/sway";
+          license = pkgs.lib.licenses.asl20;
+        };
+      };
+
+      forc-lsp = rust-platform.buildRustPackage {
+        name = "forc-lsp";
+        version = "master";
+        src = inputs.sway-src;
+        cargoHash = "sha256-bc/Xr0aBdZ+xoR0Ij72ItaoGB3X+v7cOGW+um3SCJiQ=";
+        buildAndTestSubdir = "forc-plugins/forc-lsp";
+        nativeBuildInputs = [
+          pkgs.perl # for openssl-sys
+          pkgs.pkg-config # for openssl-sys
+          rust-platform.rust.cargo
+          rust-platform.rust.rustc
+        ];
+        meta = {
+          homepage = "https://github.com/fuellabs/sway";
+          license = pkgs.lib.licenses.asl20;
+        };
+      };
 
       sway-vim = pkgs.vimUtils.buildVimPluginFrom2Nix {
         pname = "sway-vim";
@@ -74,62 +152,48 @@
           license = pkgs.lib.licenses.mit;
         };
       };
+
+      fuel = pkgs.symlinkJoin {
+        name = "fuel";
+        paths = [
+          fuel-core
+          fuel-gql-cli
+          forc
+          forc-explore
+          forc-fmt
+          forc-lsp
+        ];
+      };
+
+      default = fuel;
     };
 
     overlays = rec {
       fuel = final: prev: let
         fuelpkgs = mkPackages prev;
       in {
-        inherit (fuelpkgs) fuel-core forc forc-explore forc-fmt forc-lsp;
+        inherit (fuelpkgs) fuel-core fuel-gql-cli forc forc-explore forc-fmt forc-lsp;
         vimPlugins = prev.vimPlugins // {inherit (fuelpkgs) sway-vim;};
       };
       default = fuel;
     };
 
     mkDevShells = pkgs: rust-platform: fuelpkgs: rec {
-      fuel-core = pkgs.mkShell {
-        name = "fuel-core";
-        version = "master";
-        nativeBuildInputs = [
-          rust-platform.rust.cargo
-          rust-platform.rust.rustc
-          pkgs.pkg-config
-          pkgs.clang
-          pkgs.libclang
-        ];
-        buildInputs = [
-          pkgs.grpc-tools
-          pkgs.openssl
-        ];
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-        PROTOC = "${pkgs.grpc-tools}/bin/protoc";
+      fuel-dev = pkgs.mkShell {
+        name = "fuel-dev";
+        inputsFrom = pkgs.lib.attrValues fuelpkgs;
       };
-
-      # fuel-core = pkgs.mkShell {
-      #   name = "fuel-core-dev";
-      #   buildInputs = [
-      #     # rust-platform.rust
-      #   ];
-      # };
-      # sway = pkgs.mkShell {
-      #   name = "sway-dev";
-      #   buildInputs = [
-      #     # fuelpkgs.fuel-core
-      #     # rust-platform.rust
-      #   ];
-      # };
-      # # TODO: All dev shells combined.
-      # fuel = pkgs.mkShell {
-      #   name = "fuel-dev";
-      # };
-      # default = fuel;
+      default = fuel-dev;
     };
 
     mkOutput = system: let
       overlays = [inputs.rust-overlay.overlays.default];
       pkgs = import inputs.nixpkgs {inherit overlays system;};
       rust = pkgs.rust-bin.stable.latest.default;
-      rust-platform = pkgs.makeRustPlatform { rustc = rust; cargo = rust; };
+      rust-platform = pkgs.makeRustPlatform {
+        rustc = rust;
+        cargo = rust;
+      };
     in rec {
       packages = mkPackages pkgs rust-platform;
       devShells = mkDevShells pkgs rust-platform packages;
