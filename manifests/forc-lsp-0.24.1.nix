9ca45bd5cd4d22f5bb3aa563d4cb11e15aa339f5
mitchmindtree <mail@mitchellnordine.com>
2022-09-08 12:09:03 +1000

    Add forc versions 0.24.1

---
+++ manifests/forc-lsp-0.24.1.nix
@@ -0,0 +1,7 @@
+{
+  pname = "forc-lsp";
+  version = "0.24.1";
+  url = "https://github.com/fuellabs/sway";
+  rev = "54e1a340e6c0c9a675e8a66aab03f579d5d6b39b";
+  sha256 = "sha256-t/VFaYCANGY5CM2FFrLhNiBB5eAL/CFCZvIzigr3d1I=";
+}
