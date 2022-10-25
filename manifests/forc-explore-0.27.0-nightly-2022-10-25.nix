cbc6cafad5891de4b08d8493d8d93928b025e10b
GitHub Action <action@github.com>
2022-10-25 00:13:56 +0000

    manifest: update

---
+++ manifests/forc-explore-0.27.0-nightly-2022-10-25.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-explore";
+  version = "0.27.0";
+  date = "2022-10-25";
+  url = "https://github.com/fuellabs/sway";
+  rev = "8ae44c2ea0eb154f77e769ef3f69d7f1ab0116b9";
+  sha256 = "sha256-5yuILUm2XiRpou8bTKdfyXOYGkoKpxJ/YhsqXyJPtlk=";
+}
---
+++ manifests/forc-lsp-0.27.0.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.27.0";
+  date = "2022-10-23";
+  url = "https://github.com/fuellabs/sway";
+  rev = "40f1e79de08af109f2ebb066f51704f36094ed1e";
+  sha256 = "sha256-qIso6YBCs4HKu1fAqM0fgq0vcaOqgGeZ2CY0ZLjTm+o=";
+}
