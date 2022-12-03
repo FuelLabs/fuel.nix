1b00713ea17e0e20d43dc21e4e38eb0e171c429c
GitHub Action <action@github.com>
2022-12-03 00:09:00 +0000

    manifest: update

---
+++ manifests/forc-fmt-0.31.3-nightly-2022-12-03.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-fmt";
+  version = "0.31.3";
+  date = "2022-12-03";
+  url = "https://github.com/fuellabs/sway";
+  rev = "487770f695ee46be38d43e84ef87e7366a49a916";
+  sha256 = "sha256-OpLBWrpnSFyjczil6zvnLQHvgnG++rIjYuETawANhMo=";
+}
---
+++ manifests/forc-lsp-0.31.3-nightly-2022-12-02.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.31.3";
+  date = "2022-12-02";
+  url = "https://github.com/fuellabs/sway";
+  rev = "2188c1bf686a59c1815120fee888953a1d03ccea";
+  sha256 = "sha256-eYi3lLFgmLCOMnSnR/3trgGfZL50VbHFrjbeHQAzFp0=";
+}
