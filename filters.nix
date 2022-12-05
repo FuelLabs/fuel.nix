0af039745ce944db76e62df0a1f9df01f95b9123
mitchmindtree <mail@mitchellnordine.com>
2022-12-05 22:15:37 +1100

    Add fuel-indexer to the filters.nix

--- filters.nix
+++ filters.nix
@@ -6,9 +6,6 @@ with pkgs.lib; [
   # Specify some minimum versions that we want to support, to save ourselves
   # having to build everything since the dawn of time and from having to patch
   # old unused versions.
-  (m: m.pname != "fuel-core" || versionAtLeast m.version "0.9.0")
-  (m: m.pname != "fuel-core-client" || (versionAtLeast m.version "0.14.2" && m.date >= "2022-12-17"))
-  (m: m.pname != "fuel-gql-cli" || (versionAtLeast m.version "0.9.0" && m.date < "2022-12-17"))
   (m: m.pname != "forc" || versionAtLeast m.version "0.19.0")
   (m: m.pname != "forc-client" || versionAtLeast m.version "0.19.0")
   (m: m.pname != "forc-doc" || versionAtLeast m.version "0.29.0")
@@ -17,4 +14,8 @@ with pkgs.lib; [
   (m: m.pname != "forc-lsp" || versionAtLeast m.version "0.19.0")
   (m: m.pname != "forc-tx" || versionAtLeast m.version "0.33.1")
   (m: m.pname != "forc-wallet" || versionAtLeast m.version "0.1.0")
+  (m: m.pname != "fuel-core" || versionAtLeast m.version "0.9.0")
+  (m: m.pname != "fuel-core-client" || (versionAtLeast m.version "0.14.2" && m.date >= "2022-12-17"))
+  (m: m.pname != "fuel-gql-cli" || (versionAtLeast m.version "0.9.0" && m.date < "2022-12-17"))
+  (m: m.pname != "fuel-indexer" || versionAtLeast m.version "0.1.8")
 ]
