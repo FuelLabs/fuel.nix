0b3bd45e993f21b000c1a58fa79a62a120228de0
GitHub Action <action@github.com>
2022-12-13 00:10:01 +0000

    manifest: update

---
+++ manifests/fuel-gql-cli-0.15.0-nightly-2022-12-10.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-gql-cli";
+  version = "0.15.0";
+  date = "2022-12-10";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "5cb9ea97d23e87fc560d63758d8e31bc63bd89c7";
+  sha256 = "sha256-xelA+CICPvHwpWIdzL2RHmhjK7m3kBjUCAoUwWVsrwM=";
+}
