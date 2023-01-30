6306bb1a00918f5ad0e50d1e51e57a0080d6f2e4
mitchmindtree <mail@mitchellnordine.com>
2023-01-30 17:09:35 +1100

    Add manifests since last refresh-manifests failure

---
+++ manifests/forc-wallet-0.1.2-nightly-2023-01-24.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.1.2";
+  date = "2023-01-24";
+  url = "https://github.com/fuellabs/forc-wallet";
+  rev = "a73f1b81681ab07da4da9596e58d44ea95a46bff";
+  sha256 = "sha256-MyJfAVpXTtKcHABc88j+1Y13lcQ2/vRoVgpHa7kGPyc=";
+}
