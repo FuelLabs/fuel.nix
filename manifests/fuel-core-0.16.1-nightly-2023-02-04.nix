5aef3bb543693978f6ec7a38895e5284c7b598de
mitchmindtree <mail@mitchellnordine.com>
2023-02-04 13:53:52 +1100

    `fuel-core` now requires Rust 1.67
    Updates the `rust-overlay` flake input to bring in support for 1.67.
    
    Also udpates a fuel-core patch to ensure tests aren't run unnecessarily
    for new fuel-core-client crate.
    
    Also includes manifests for previous failed CI job.

---
+++ manifests/fuel-core-0.16.1-nightly-2023-02-04.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.16.1";
+  date = "2023-02-04";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "580b2212bd5fa9870c9fef11e0ad72f373925e78";
+  sha256 = "sha256-D44myJ00lNbIdKid47e387YHlkOOZdc35b/yM+FRMTs=";
+}
