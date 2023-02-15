1ad5264bd73c463c350dd7e34538f1db411ec379
mitchmindtree <mail@mitchellnordine.com>
2023-02-15 19:36:14 +1000

    Add the beta-3 milestone (#49)
    Tentatively opening this, anticipating updating the Sway commit once a
    patch release drops.

--- milestones.nix
+++ milestones.nix
@@ -19,4 +19,14 @@
     fuel-indexer = "c2425c8b63f01ef1b540ff3e5832ebdc018b951d";
     sway = "c32b0759d25c0b515cbf535f9fb9b8e6fda38ff2";
   };
+
+  # Commits sourced from:
+  # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-beta-3.toml
+  beta-3 = {
+    forc-explorer = "4bb7392eed085ee3a6795b98ea25392b3f41ade8";
+    forc-wallet = "543bc1f4d7badd569c22dff88db2a988adee9d4e";
+    fuel-core = "ce64223296537cecf5b8c2e2892e31e2f98fc9a5";
+    fuel-indexer = "c55a0e34600d8f7c888a95dd3c0adb8fadfaf024";
+    sway = "b6f19a3be7b2fb5ef88e358a926854dac10cb281";
+  };
 }
