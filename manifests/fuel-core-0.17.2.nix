2766e5f27d1f088f1f6c1e2f9a1f9defa11e6fb8
mitchmindtree <mail@mitchellnordine.com>
2023-02-21 16:12:53 +1000

    Add `openssl` to `buildInputs` for indexer packages (#51)
    This requirement appears to have been introduced about a week ago.
    
    This PR also includes the manifests generated since CI started failing.

---
+++ manifests/fuel-core-0.17.2.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.17.2";
+  date = "2023-02-14";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "95a9608d61fee2f30446bb59d967c3dad2655fef";
+  sha256 = "sha256-iQGu2qnySx2uYwc15mWe70HWLnKZjtfGX0p/BqlNVXA=";
+}
---
+++ manifests/fuel-core-client-0.17.2-nightly-2023-02-20.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.17.2";
+  date = "2023-02-20";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "0cefc99bbf4e14c6c43702f33954efebd3ebdc76";
+  sha256 = "sha256-V4O0xG/BejzdjlXLbZ4w6Er/eVHaVDi23V/Ozdg6AP4=";
+}
