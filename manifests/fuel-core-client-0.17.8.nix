702a77c7f40b05a56aa88adf2f98655f41f2aa31
GitHub Action <action@github.com>
2023-04-06 00:27:31 +0000

    manifest: update

---
+++ manifests/fuel-core-client-0.17.8.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.17.8";
+  date = "2023-04-05";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "1cff795d2115d3383aca577032be1ebe837a4688";
+  sha256 = "sha256-PKFBsqKtsSYlXusK/rt3QueskLQ90gEf5dUUJWbY5VQ=";
+}
