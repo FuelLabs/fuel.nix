82e0b40955adcc6bcf6ea9b7b8c89fd36c527ce1
GitHub Action <action@github.com>
2023-05-20 00:27:08 +0000

    manifest: update

---
+++ manifests/forc-fmt-0.39.0-nightly-2023-05-20.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-fmt";
+  version = "0.39.0";
+  date = "2023-05-20";
+  url = "https://github.com/fuellabs/sway";
+  rev = "3445cd1f587e95b5cdd056014d49f2d329d5580a";
+  sha256 = "sha256-tETXu1zZSVI4dH2djrm6vOLhvRsgQFP4Ocvrj2rs6Ns=";
+}
