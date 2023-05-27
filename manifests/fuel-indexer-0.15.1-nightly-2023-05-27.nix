d959cd6eaa84af6f65262626397422a7ac18c6ad
GitHub Action <action@github.com>
2023-05-27 00:31:31 +0000

    manifest: update

---
+++ manifests/fuel-indexer-0.15.1-nightly-2023-05-27.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-indexer";
+  version = "0.15.1";
+  date = "2023-05-27";
+  url = "https://github.com/fuellabs/fuel-indexer";
+  rev = "7dc5f47728ad3ee8fd515fc55ef980fab13420d2";
+  sha256 = "sha256-BgJ4xVPmKNH4UdQaTzNMkxHGqT26sYVouOe0/9ad2+U=";
+}
