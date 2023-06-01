e7dbd14899ac0067e1b297b68c8c20ab192278c2
mitchmindtree <mail@mitchellnordine.com>
2023-06-01 15:33:08 +1000

    RM 2023-05-30 nightlies to find the most recent working versions

--- manifests/fuel-core-0.18.1-nightly-2023-05-30.nix
+++
@@ -1,8 +0,0 @@
-{
-  pname = "fuel-core";
-  version = "0.18.1";
-  date = "2023-05-30";
-  url = "https://github.com/fuellabs/fuel-core";
-  rev = "ea438b7d8f9404f185239f3f6896bd4c1dd33701";
-  sha256 = "sha256-cJnwue+SjqLhgMqngHrupcsAHBHfUKG4hfVmFvwWoBo=";
-}
