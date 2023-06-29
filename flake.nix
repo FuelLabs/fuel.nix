17e85ee275ab448ed04b4a642a82a5e21aa49069
kayagokalp <kaya.gokalp@fuel.sh>
2023-06-29 15:34:15 +0100

    update lock file

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   inputs = {
     nixpkgs = {
       # Until fuel binaries bump their git2-rs version, build with libgit2-1.5.1 and pin nixpkgs to the correct revision
-      url = "github:NixOS/nixpkgs?branch=nixos-unstable?rev=8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
+      url = "github:NixOS/nixpkgs/8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8";
     };
     rust-overlay = {
       url = "github:oxalica/rust-overlay/master";
