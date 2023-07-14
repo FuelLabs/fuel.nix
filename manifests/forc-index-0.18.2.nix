886d0e52bb126507e1aa543f56e579246af1db24
GitHub Action <action@github.com>
2023-07-14 02:13:03 +0000

    manifest: update

---
+++ manifests/forc-index-0.18.2.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-index";
+  version = "0.18.2";
+  date = "2023-07-13";
+  url = "https://github.com/fuellabs/fuel-indexer";
+  rev = "c73a40da56bcc2fabd92111f42d14631659635d7";
+  sha256 = "sha256-e8Rv9oQ8Kgs2fcndFp3I/MyFViDIdS2G+E9Ky/qCGdE=";
+}
