16065050bbdce8c9fea444fc4cdb2e01f4ddbaf3
GitHub Action <action@github.com>
2023-08-12 03:11:09 +0000

    manifest: update

---
+++ manifests/forc-wallet-0.2.4-nightly-2023-08-12.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.2.4";
+  date = "2023-08-12";
+  url = "https://github.com/fuellabs/forc-wallet";
+  rev = "755404c32dc3955b72304b200449e0cc9759f6ca";
+  sha256 = "sha256-ZWSYcTzYf+Kd/FSKGP7Ac3eyHacLFXnEJQml3DTc/sA=";
+}
---
+++ manifests/fuel-core-client-0.19.2-nightly-2023-08-12.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.19.2";
+  date = "2023-08-12";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "526a9eb3e678449e56330d48d538761a9870b012";
+  sha256 = "sha256-jjFuAToRHDE5BnuObkldlOgVK+S9iwvCe/1VOHXK8cM=";
+}
