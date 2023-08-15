6feccacce12a6c960083059962759103b2bcc5a7
Sophie <sophiedankel@gmail.com>
2023-08-15 16:53:54 -0700

    list-nix-store.sh

--- script/cachix-push-paths.sh
+++ script/cachix-push-paths.sh
@@ -1,16 +1,8 @@
 #!/usr/bin/env zsh
 set -euo pipefail
 
-# Borrowed from https://github.com/cachix/cachix-action/blob/master/dist/main/push-paths.sh
+# Based on https://github.com/cachix/cachix-action/blob/master/dist/main/push-paths.sh
 
-cachix=$1 cachixArgs=${2:--j8} cache=$3 pathsToPush=$4 pushFilter=$5
+pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh))
 
-if [[ $pathsToPush == "" ]]; then
-    pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh))
-
-    if [[ $pushFilter != "" ]]; then
-        pathsToPush=$(echo "$pathsToPush" | grep -vEe "$pushFilter")
-    fi
-fi
-
-echo "$pathsToPush" | "$cachix" push $cachixArgs "$cache"
+echo "$pathsToPush" | cachix push "fuellabs"
