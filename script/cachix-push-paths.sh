0ec2023b3fa4b5a6fc98f20986e11b84215b3255
Sophie <sophiedankel@gmail.com>
2023-08-15 16:42:52 -0700

    push_paths script

---
+++ script/cachix-push-paths.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Borrowed from https://github.com/cachix/cachix-action/blob/master/dist/main/push-paths.sh
+
+cachix=$1 cachixArgs=${2:--j8} cache=$3 pathsToPush=$4 pushFilter=$5
+
+if [[ $pathsToPush == "" ]]; then
+    pathsToPush=$(comm -13 <(sort /tmp/store-path-pre-build) <("$(dirname "$0")"/list-nix-store.sh))
+
+    if [[ $pushFilter != "" ]]; then
+        pathsToPush=$(echo "$pathsToPush" | grep -vEe "$pushFilter")
+    fi
+fi
+
+echo "$pathsToPush" | "$cachix" push $cachixArgs "$cache"
