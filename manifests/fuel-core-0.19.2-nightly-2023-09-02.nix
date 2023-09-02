634a33d769d5d1129cace03cbd2196509fb08fcc
GitHub Action <action@github.com>
2023-09-02 02:55:59 +0000

    manifest: update

---
+++ manifests/fuel-core-0.19.2-nightly-2023-09-02.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.19.2";
+  date = "2023-09-02";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "7005730e0432a179f1d1c2d946b396bb30a5ce97";
+  sha256 = "sha256-71vLvX5ftyYMDWRGlHbrGbCWNVHxxlZb5HzOS2HV/0c=";
+}
---
+++ manifests/fuel-indexer-0.20.7-nightly-2023-09-02.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-indexer";
+  version = "0.20.7";
+  date = "2023-09-02";
+  url = "https://github.com/fuellabs/fuel-indexer";
+  rev = "30af7969962e3ccc936545810f51bd74a8b50f8d";
+  sha256 = "sha256-K5xQ29FADuEUJLaPpR/Uy69JWlz1wS7S7qGvkUTBM6Y=";
+}
