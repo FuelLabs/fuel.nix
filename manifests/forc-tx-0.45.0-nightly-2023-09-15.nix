815aba6aa28dce2373fa1381f11ff81231a96378
GitHub Action <action@github.com>
2023-09-15 02:43:29 +0000

    manifest: update

---
+++ manifests/forc-tx-0.45.0-nightly-2023-09-15.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-tx";
+  version = "0.45.0";
+  date = "2023-09-15";
+  url = "https://github.com/fuellabs/sway";
+  rev = "e75f14b03636bc96751a6760304a1a6d3eb5937d";
+  sha256 = "sha256-mTqo8B7XUbherC9XBpDtlEtjewCxEzn6yC2kmiCFjVI=";
+}
