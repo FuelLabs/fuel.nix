908d1c90a5a8e875a523c3405fd502c04ddc9e0a
GitHub Action <action@github.com>
2023-09-23 04:55:58 +0000

    manifest: update

---
+++ manifests/forc-client-0.46.0-nightly-2023-09-23.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-client";
+  version = "0.46.0";
+  date = "2023-09-23";
+  url = "https://github.com/fuellabs/sway";
+  rev = "15aa402875d30c6ca56f48aa0a9f6ed28c2d3bd4";
+  sha256 = "sha256-15sQAOrATo5HK2PmAzwWmYMFP7f4U6xd0vc5HDWmLpo=";
+}
