e8202e68f7ff3521573dc236360da62bfd8e1bad
GitHub Action <action@github.com>
2023-10-18 05:15:22 +0000

    manifest: update

---
+++ manifests/forc-0.46.1-nightly-2023-10-18.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc";
+  version = "0.46.1";
+  date = "2023-10-18";
+  url = "https://github.com/fuellabs/sway";
+  rev = "0abaf0413d1d7c1546a40b7e7a51ab7ca07777ae";
+  sha256 = "sha256-YvW+Nt8+VqCuemHPj1sryimzwcEdWEYNYNmWyGnL8EA=";
+}
