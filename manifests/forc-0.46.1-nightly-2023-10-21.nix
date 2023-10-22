27441f557d8d760693b55905cc664965ed13851b
GitHub Action <action@github.com>
2023-10-22 05:40:54 +0000

    manifest: update

---
+++ manifests/forc-0.46.1-nightly-2023-10-21.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc";
+  version = "0.46.1";
+  date = "2023-10-21";
+  url = "https://github.com/fuellabs/sway";
+  rev = "4a09a4be85d72a5849b91fcecd7609717a8a9f46";
+  sha256 = "sha256-XBGK5zMfqLmnHjfWQFsHmcNTDGODWmu3mbTfioBhZ8k=";
+}
---
+++ manifests/fuel-indexer-0.21.2-nightly-2023-10-20.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-indexer";
+  version = "0.21.2";
+  date = "2023-10-20";
+  url = "https://github.com/fuellabs/fuel-indexer";
+  rev = "18f846cde049ad37623d9f66db8adbd549b4c6b4";
+  sha256 = "sha256-+S5bfKa8IGzV42YSTmGXTxONqVq0vtN6V6/H2+8oKxQ=";
+}
