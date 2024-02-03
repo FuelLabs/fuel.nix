5221a9124f29b9adebefc5a78d034febf21a4fd6
Kaya Gokalp <kayagokalp123@gmail.com>
2024-02-02 17:02:19 -0800

    fix: broken old nightlies fixed

---
+++ manifests/fuel-core-client-0.22.0-nightly-2024-02-03.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.22.0";
+  date = "2024-02-03";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "7e54cb8f107a7153596a2b451d4e5656a1805b1d";
+  sha256 = "sha256-YqiGbFdIDz1XZvmiig1H/yoNspTdMiJey1FwW9xDdDQ=";
+}
