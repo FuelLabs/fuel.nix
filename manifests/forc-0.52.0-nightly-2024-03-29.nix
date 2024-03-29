252a5e4112b2e1beb988f7f0a70dca956b6b4fd4
GitHub Action <action@github.com>
2024-03-29 02:33:19 +0000

    manifest: update

---
+++ manifests/forc-0.52.0-nightly-2024-03-29.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc";
+  version = "0.52.0";
+  date = "2024-03-29";
+  url = "https://github.com/fuellabs/sway";
+  rev = "195acc6be5262b0711f34f6ec5583d3502d06a7a";
+  sha256 = "sha256-izbcCn37EHix5/ndXcFpS1uCNAGPo+4RvLljH1lFjUc=";
+}
---
+++ manifests/forc-wallet-0.4.3-nightly-2024-03-29.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.4.3";
+  date = "2024-03-29";
+  url = "https://github.com/fuellabs/forc-wallet";
+  rev = "f1fa9300041d8d9b6bbb8d31711a581fe3110991";
+  sha256 = "sha256-9FL4jWToJu7iq7tu4HZXM0POrlUQnAeB7Q6YAqLP5B0=";
+}
