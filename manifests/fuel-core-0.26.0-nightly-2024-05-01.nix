d5ed1501034d72e4b4421b735af1cdb8ea13c10d
GitHub Action <action@github.com>
2024-05-03 11:07:33 +0000

    manifest: update

---
+++ manifests/fuel-core-0.26.0-nightly-2024-05-01.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.26.0";
+  date = "2024-05-01";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "9fddeccb4d112c148f6793bc3d21131a13778a25";
+  sha256 = "sha256-UdsSCqvUfgHFvQC5aGFjpp/agqunYUprJ1r1CViL+h8=";
+}
---
+++ manifests/fuel-core-0.26.0-nightly-2024-05-03.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.26.0";
+  date = "2024-05-03";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "fd6874530558199632f51f54b2892fceb950be85";
+  sha256 = "sha256-bnLvXdQJOuzaazkqRGdCDcodvYT5Iu13pXann9ypU3E=";
+}
