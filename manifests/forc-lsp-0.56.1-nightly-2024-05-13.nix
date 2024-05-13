6484e43cf66ba9ae5d658f9d49b04fb3bc1b142a
GitHub Action <action@github.com>
2024-05-13 02:27:56 +0000

    manifest: update

---
+++ manifests/forc-lsp-0.56.1-nightly-2024-05-13.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.56.1";
+  date = "2024-05-13";
+  url = "https://github.com/fuellabs/sway";
+  rev = "a7d35d3f09aef84ffa85a650d88bc52d6f593d77";
+  sha256 = "sha256-8HjqSMg7nxcPpQf+47zgSjyD1DUOTjryzK8RbVweDDM=";
+}
