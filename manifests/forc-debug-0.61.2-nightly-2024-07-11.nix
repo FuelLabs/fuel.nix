2a4641c76539a80dcffd3182600dac2e4fadfce9
GitHub Action <action@github.com>
2024-07-11 02:33:11 +0000

    manifest: update

---
+++ manifests/forc-debug-0.61.2-nightly-2024-07-11.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-debug";
+  version = "0.61.2";
+  date = "2024-07-11";
+  url = "https://github.com/fuellabs/sway";
+  rev = "250666d3de43439dd4026ef844616c448b6ffde7";
+  sha256 = "sha256-/ImICQ7EPniQw+1QULo+rw0OgQD0Sm/JUgGIfmKDlpQ=";
+}
---
+++ manifests/forc-wallet-0.8.2.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.8.2";
+  date = "2024-07-10";
+  url = "https://github.com/fuellabs/forc-wallet";
+  rev = "76e9796cbee4426312e8e840d022cd695ca64fab";
+  sha256 = "sha256-ais2tFxNIRsY0fELoxnBAF+Xf1FuG8BlGY9Hhv9P3/o=";
+}
