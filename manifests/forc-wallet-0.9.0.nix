b8f20d4a140bdf4c1ec56b79277238354abce8d5
GitHub Action <action@github.com>
2024-08-13 19:45:48 +0000

    ci: refresh manifests for patch PR

---
+++ manifests/forc-wallet-0.9.0.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.9.0";
+  date = "2024-08-12";
+  url = "https://github.com/fuellabs/forc-wallet";
+  rev = "29d9b25c2ce7a273618945d561148e3034e47441";
+  sha256 = "sha256-gsC0a2NAMbgKQ2hV7Zn+6K+lFKHgOwaYu0udFxQ4uiE=";
+}
