177250000ba62e0bebc6f06bffbc4d800eeffc61
Sophie <sophiedankel@gmail.com>
2024-08-14 18:44:36 -0700

    chore: Refresh manifests

---
+++ manifests/fuel-core-client-0.32.1-nightly-2024-08-11.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.32.1";
+  date = "2024-08-11";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "a9e5e89f5fb38e3b3d6e6bdfe1ad339a01f2f3b9";
+  sha256 = "sha256-Dt6S+ABQR0L8BWHWmjBFMmYaPSokNXA5Ns2eX6g2NUo=";
+}
