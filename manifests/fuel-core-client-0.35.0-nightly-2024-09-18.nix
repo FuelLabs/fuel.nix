c68647684fbfe47a559497974beb9f31918bdada
GitHub Action <action@github.com>
2024-10-02 02:30:14 +0000

    manifest: update

--- manifests/fuel-core-client-0.35.0-nightly-2024-09-18.nix
+++ manifests/fuel-core-client-0.35.0-nightly-2024-09-18.nix
@@ -3,6 +3,6 @@
   version = "0.35.0";
   date = "2024-09-18";
   url = "https://github.com/fuellabs/fuel-core";
-  rev = "20f3091f46529d63d225db3749d2934c47dcb3de";
-  sha256 = "sha256-uLD22wP31B7l/QGk6RzRraxvw9eDtfCpNlrKif+ldm4=";
+  rev = "b5d5ff8d13f16a0bd53e79ee0e8f5438971be2b9";
+  sha256 = "sha256-teTRhUa9KhmxvcolgQ8B7UDlyN8pbH497tqxvTXymwE=";
 }
