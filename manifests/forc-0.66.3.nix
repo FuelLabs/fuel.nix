68cfffc3e38408717b612b02024b6c77d0b60d70
GitHub Action <action@github.com>
2024-10-31 03:41:38 +0000

    manifest: update

---
+++ manifests/forc-0.66.3.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc";
+  version = "0.66.3";
+  date = "2024-10-30";
+  url = "https://github.com/fuellabs/sway";
+  rev = "5b7d720cf97e88eedc3c069b617ee7493bb16637";
+  sha256 = "sha256-35bHX4r/WB3peaTJ5qK2TV3xRtxx5nBxUpDA+4oOlm4=";
+}
---
+++ manifests/fuel-core-client-0.40.0-nightly-2024-10-31.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.40.0";
+  date = "2024-10-31";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "ec41f562343bc65ea5e3b8cda3fa49a60985b3d3";
+  sha256 = "sha256-BUWdjazabJgacQgE7JbyWX2xU5rGZhKm0jdk2junipE=";
+}
