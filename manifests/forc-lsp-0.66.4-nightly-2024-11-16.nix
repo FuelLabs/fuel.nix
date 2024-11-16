ae7b00060ecc29c7cdd2fe77b606b04f791107d2
GitHub Action <action@github.com>
2024-11-16 03:46:13 +0000

    manifest: update

---
+++ manifests/forc-lsp-0.66.4-nightly-2024-11-16.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.66.4";
+  date = "2024-11-16";
+  url = "https://github.com/fuellabs/sway";
+  rev = "1dfca80d7cc7bba9aa936b9dfbab3dbf8c3bb760";
+  sha256 = "sha256-CgJnCUueJ2uVxLOPjjTsKX/AHV8sdK87LG0TrbYnjfg=";
+}
---
+++ manifests/fuel-core-0.40.0-nightly-2024-11-16.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.40.0";
+  date = "2024-11-16";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "3f8ea1e85e0048e76811cbc333e29ccda486d2b9";
+  sha256 = "sha256-8aVQFG8zOcMKf6bD6zbjh4z2p5EvFWYk1ESTD3ohdPc=";
+}
