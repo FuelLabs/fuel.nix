d04de1bf17d77d53d8bac61c2d07e4ea972e3b86
Sophie Dankel <47993817+sdankel@users.noreply.github.com>
2025-01-07 15:41:19 -0800

    Update nightly date for manifest refresh (#155)
    Makes it so only nightlies after 01-01-2024 receive new patches. This
    cuts the time to refresh manifests down from ~3 hours to [43
    min](https://github.com/FuelLabs/fuel.nix/actions/runs/12659594373)
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ manifests/forc-client-0.48.1-nightly-2024-01-01.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-client";
+  version = "0.48.1";
+  date = "2024-01-01";
+  url = "https://github.com/fuellabs/sway";
+  rev = "910e33bd015e2ea4571e042a856a0cd5c910b93a";
+  sha256 = "sha256-KmpIgbuH/Worse7RMsiR1UsSrwAZ1X4nIZG+H1fEoz4=";
+}
---
+++ manifests/forc-wallet-0.4.0-nightly-2024-01-01.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.4.0";
+  date = "2024-01-01";
+  url = "https://github.com/fuellabs/forc-wallet";
+  rev = "36ecad0a149929e565542dbc0c977e26f91d593a";
+  sha256 = "sha256-wBauC198giQQ1uPPLbQUx8wLsmwTo2RbsTD4TZFRDVI=";
+}
