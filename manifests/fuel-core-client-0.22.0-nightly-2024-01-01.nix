f51a1d700840144b30564f4031fa459d8c7c2d65
GitHub Action <action@github.com>
2025-01-07 22:10:03 +0000

    ci: refresh manifests for patch PR

---
+++ manifests/fuel-core-client-0.22.0-nightly-2024-01-01.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.22.0";
+  date = "2024-01-01";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "0daa63594f670ad2c6d2028f9cc3f61bf918426c";
+  sha256 = "sha256-W890Wxz+ZiJZyb4b1LgmOBS7u0N+d4KE4Mmq+Glz0BY=";
+}
