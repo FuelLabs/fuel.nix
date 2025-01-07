03cba06747b910290b7eb1e0a2afe01d9423a4ae
Sophie Dankel <47993817+sdankel@users.noreply.github.com>
2025-01-07 05:52:27 -0800

    fix: patch rust v1.81 as of >2024-12-30 (#153)
    Patches the version of rust, increases the timeout for the
    refresh-manifests actions, and moves them to run in build-jet. As a
    follow up, we should adjust the script to only refresh more recent
    nightly builds to reduce the time it takes.
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ manifests/forc-0.66.5-nightly-2025-01-07.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc";
+  version = "0.66.5";
+  date = "2025-01-07";
+  url = "https://github.com/fuellabs/sway";
+  rev = "8fe2bd6ed267d94e172350d0835de5a11d796cea";
+  sha256 = "sha256-AoUm642EeZxVdn5XUUlfCwQjQLuftPzPLpUmVzFhuH4=";
+}
