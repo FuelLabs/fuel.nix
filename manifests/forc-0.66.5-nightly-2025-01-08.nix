90284f3d0c715a8bff1e63d04bfaf097a7bfa79d
Sophie <47993817+sdankel@users.noreply.github.com>
2025-01-09 13:41:14 -0800

    Revert "ci: refresh manifests for patch PR"
    This reverts commit 4eba344522508779506f3da07089dc484c34c1bc.

--- manifests/forc-0.66.5-nightly-2025-01-08.nix
+++
@@ -1,8 +0,0 @@
-{
-  pname = "forc";
-  version = "0.66.5";
-  date = "2025-01-08";
-  url = "https://github.com/fuellabs/sway";
-  rev = "23576769d151167870a1454ce112f4f9d7271d8c";
-  sha256 = "sha256-stusfXL0gE3QXPa4QX+qKxDq/ZHahfKLMXVpLRiaVh0=";
-}
--- manifests/fuel-core-client-0.40.2-nightly-2025-01-09.nix
+++
@@ -1,8 +0,0 @@
-{
-  pname = "fuel-core-client";
-  version = "0.40.2";
-  date = "2025-01-09";
-  url = "https://github.com/fuellabs/fuel-core";
-  rev = "01fb27aa9c89bec6f8168a04755eb66aadb86ebb";
-  sha256 = "sha256-qOX+hg8tLtyEZmOi3NocWqaO+AsrHS3wQv0RJi7gHU0=";
-}
