88e25296370c524677b964c68fafc13620f040f5
GitHub Action <action@github.com>
2025-03-05 02:21:10 +0000

    manifest: update

---
+++ manifests/forc-lsp-0.66.7-nightly-2025-03-05.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.66.7";
+  date = "2025-03-05";
+  url = "https://github.com/fuellabs/sway";
+  rev = "74b724da20b8ab3ab95129456a785f744983cfe4";
+  sha256 = "sha256-/AwsRmyroqnODMRiGELQIvUY0u0nthTBI3qiA6puNdE=";
+}
---
+++ manifests/fuel-core-client-0.41.7-nightly-2025-03-05.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.41.7";
+  date = "2025-03-05";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "1f5230645ad82c81d0117f3c209b10aaa1961d2f";
+  sha256 = "sha256-g0lhcSi3+3xNqqTOA417B8Q5CRaf2TdiG3k2mScrBq4=";
+}
