b0c4119794b3502392a72d47f66038631b071b66
kaya <kaya.gokalp@fuel.sh>
2025-04-12 18:16:53 -0700

    chore: use rust 1.85 as of 2025-04-10 for fuel-core (#171)
    Bumps rust version for
    https://github.com/FuelLabs/fuel-core/commit/2a7e031fca28c751215030f719c99cb6a82d30f3
    
    ---------
    
    Co-authored-by: GitHub Action <action@github.com>

---
+++ manifests/fuel-core-client-0.43.0-nightly-2025-04-12.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.43.0";
+  date = "2025-04-12";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "2a7e031fca28c751215030f719c99cb6a82d30f3";
+  sha256 = "sha256-BnGzIGYbXIcY8KzfdsYBnFxSNXDsUxeG70e/lwqS+fQ=";
+}
