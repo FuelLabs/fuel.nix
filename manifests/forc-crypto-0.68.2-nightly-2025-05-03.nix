05891b4ae459dacd8620a8821406dc6618c84724
GitHub Action <action@github.com>
2025-05-03 02:33:10 +0000

    manifest: update

---
+++ manifests/forc-crypto-0.68.2-nightly-2025-05-03.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.68.2";
+  date = "2025-05-03";
+  url = "https://github.com/fuellabs/sway";
+  rev = "324f04fbab358a8e7eb729d03e0293605755ba5a";
+  sha256 = "sha256-0gh3Yi3GUWf6QmXUtVPyIzJd8MjGBJtI5qLUu40txHM=";
+}
