3d1399204c5b2ba6c29dac204c9c2b41f8eca2bf
GitHub Action <action@github.com>
2025-05-08 01:59:50 +0000

    manifest: update

---
+++ manifests/forc-debug-0.68.2-nightly-2025-05-08.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-debug";
+  version = "0.68.2";
+  date = "2025-05-08";
+  url = "https://github.com/fuellabs/sway";
+  rev = "8fb7a55f2145f2012d2d2df82b790e44a76106a1";
+  sha256 = "sha256-Cq9aCSoap1QiqmIDLeqnAP6Uu1vx0A6tsDfD3jVz3W8=";
+}
