03046dd1f28876dbe900e76f440f1d800881ba59
GitHub Action <action@github.com>
2025-05-09 02:29:25 +0000

    manifest: update

---
+++ manifests/forc-fmt-0.68.2-nightly-2025-05-09.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-fmt";
+  version = "0.68.2";
+  date = "2025-05-09";
+  url = "https://github.com/fuellabs/sway";
+  rev = "bd3813606ad2c4685ae979c7d4443573642c929d";
+  sha256 = "sha256-SQX3NYAWZDIGwYpzgx8i+4IWemHPn4Y+L1m4AY7el4Q=";
+}
---
+++ manifests/fuel-core-0.43.2-nightly-2025-05-09.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.43.2";
+  date = "2025-05-09";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "af4e7b7d2501ac7f32443c167d3d8e72165363ea";
+  sha256 = "sha256-pIQneElsJpc/sy7jLFkDhiHCs1XOkNgfBcoNyg8TYAw=";
+}
