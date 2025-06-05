3ff6e63c05a04485a1504dc33bcc5dbb0cdae45d
GitHub Action <action@github.com>
2025-06-05 02:21:01 +0000

    manifest: update

---
+++ manifests/forc-doc-0.68.7.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-doc";
+  version = "0.68.7";
+  date = "2025-06-04";
+  url = "https://github.com/fuellabs/sway";
+  rev = "58bf38ef9c737edbd677d2e0c5e5f32e8353ea00";
+  sha256 = "sha256-1T2N3nbh16xaewOrHjAtnbWvMADJcsT6DToPOmGyh/M=";
+}
---
+++ manifests/forc-lsp-0.68.6-nightly-2025-06-05.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.68.6";
+  date = "2025-06-05";
+  url = "https://github.com/fuellabs/sway";
+  rev = "58bf38ef9c737edbd677d2e0c5e5f32e8353ea00";
+  sha256 = "sha256-1T2N3nbh16xaewOrHjAtnbWvMADJcsT6DToPOmGyh/M=";
+}
