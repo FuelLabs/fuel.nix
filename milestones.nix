a22da2308298859df5b671402a71fb743adff239
Kaya Gokalp <20915464+kayagokalp@users.noreply.github.com>
2025-07-28 19:49:25 -0700

    chore: update milestones

--- milestones.nix
+++ milestones.nix
@@ -6,7 +6,7 @@
   # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-testnet.toml
   testnet = {
     forc-wallet = "9fea98d095d72c771f6a6f81f901fa4f8d5ece52";
-    fuel-core = "85b2356d510a30cffaa8be7015203bb8ac30fee6";
+    fuel-core = "8e34930a63329ce60bb361d38e25f6b9a7429a05";
     sway = "b68a3861db866f33078b3c966eaf5b3379e716cf";
   };
 
@@ -14,7 +14,7 @@
   # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-mainnet.toml
   ignition = {
     forc-wallet = "9fea98d095d72c771f6a6f81f901fa4f8d5ece52";
-    fuel-core = "8e34930a63329ce60bb361d38e25f6b9a7429a05";
+    fuel-core = "85b2356d510a30cffaa8be7015203bb8ac30fee6";
     sway = "b68a3861db866f33078b3c966eaf5b3379e716cf";
   };
 
@@ -22,7 +22,7 @@
   # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-mainnet.toml
   mainnet = {
     forc-wallet = "9fea98d095d72c771f6a6f81f901fa4f8d5ece52";
-    fuel-core = "8e34930a63329ce60bb361d38e25f6b9a7429a05";
+    fuel-core = "85b2356d510a30cffaa8be7015203bb8ac30fee6";
     sway = "b68a3861db866f33078b3c966eaf5b3379e716cf";
   };
 }
