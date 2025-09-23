FuelLabs/fuel.nix

commit d4b24b905cbf8aba39c17673d9fb4d9ed2bad743

add missing plugins: `forc-call`, `forc-node`, `forc-migrate`, `forc-publish` (#191)

filters.nix
@@ -15,6 +15,10 @@ with pkgs.lib; [
   (m: m.pname != "forc-lsp" || versionAtLeast m.version "0.19.0")
   (m: m.pname != "forc-tx" || versionAtLeast m.version "0.33.1")
   (m: m.pname != "forc-wallet" || versionAtLeast m.version "0.1.0")
+  (m: m.pname != "forc-call" || versionAtLeast m.version "0.69.1")
+  (m: m.pname != "forc-migrate" || versionAtLeast m.version "0.69.1")
+  (m: m.pname != "forc-node" || versionAtLeast m.version "0.69.1")
+  (m: m.pname != "forc-publish" || versionAtLeast m.version "0.69.1")
   (m: m.pname != "fuel-core" || versionAtLeast m.version "0.9.0")
   (m: m.pname != "fuel-core-client" || (versionAtLeast m.version "0.14.2" && m.date >= "2022-12-17"))
   (m: m.pname != "fuel-gql-cli" || (versionAtLeast m.version "0.9.0" && m.date < "2022-12-17"))

flake.nix
@@ -240,14 +240,19 @@
 
       sway-dev = pkgs.mkShell {
         name = "sway-dev";
-        inputsFrom = with fuelpkgs; [
-          forc-nightly
-          forc-client-nightly
-          forc-doc-nightly
-          forc-fmt-nightly
-          forc-lsp-nightly
-          forc-tx-nightly
-        ];
+        inputsFrom = with fuelpkgs;
+          [
+            forc-nightly
+            forc-client-nightly
+            forc-doc-nightly
+            forc-fmt-nightly
+            forc-lsp-nightly
+            forc-tx-nightly
+          ]
+          ++ (pkgs.lib.optional (fuelpkgs ? forc-call-nightly) forc-call-nightly)
+          ++ (pkgs.lib.optional (fuelpkgs ? forc-migrate-nightly) forc-migrate-nightly)
+          ++ (pkgs.lib.optional (fuelpkgs ? forc-node-nightly) forc-node-nightly)
+          ++ (pkgs.lib.optional (fuelpkgs ? forc-publish-nightly) forc-publish-nightly);
         buildInputs = with fuelpkgs; [fuel-core fuel-gql-cli];
       };
 

patches.nix
@@ -10,6 +10,10 @@
     "forc-fmt"
     "forc-lsp"
     "forc-tx"
+    "forc-call"
+    "forc-migrate"
+    "forc-node"
+    "forc-publish"
   ];
 in [
   # By default, most packages have their `Cargo.lock` file in the repo root.

commit fe217f39d1fb7574eba806ba43f00c043c7a0dae

Remove forc-call package entry to unblock CI (#192)
Regression introduced in #191 packaged `forc-call` as a standalone
crate, but the Sway repo only ships that binary inside forc-client. This
PR removes forc-call as it's already included in forc-client.

---------

Co-authored-by: GitHub Action <action@github.com>

filters.nix
@@ -15,7 +15,6 @@ with pkgs.lib; [
   (m: m.pname != "forc-lsp" || versionAtLeast m.version "0.19.0")
   (m: m.pname != "forc-tx" || versionAtLeast m.version "0.33.1")
   (m: m.pname != "forc-wallet" || versionAtLeast m.version "0.1.0")
-  (m: m.pname != "forc-call" || versionAtLeast m.version "0.69.1")
   (m: m.pname != "forc-migrate" || versionAtLeast m.version "0.69.1")
   (m: m.pname != "forc-node" || versionAtLeast m.version "0.69.1")
   (m: m.pname != "forc-publish" || versionAtLeast m.version "0.69.1")

flake.nix
@@ -249,7 +249,6 @@
             forc-lsp-nightly
             forc-tx-nightly
           ]
-          ++ (pkgs.lib.optional (fuelpkgs ? forc-call-nightly) forc-call-nightly)
           ++ (pkgs.lib.optional (fuelpkgs ? forc-migrate-nightly) forc-migrate-nightly)
           ++ (pkgs.lib.optional (fuelpkgs ? forc-node-nightly) forc-node-nightly)
           ++ (pkgs.lib.optional (fuelpkgs ? forc-publish-nightly) forc-publish-nightly);

patches.nix
@@ -10,7 +10,6 @@
     "forc-fmt"
     "forc-lsp"
     "forc-tx"
-    "forc-call"
     "forc-migrate"
     "forc-node"
     "forc-publish"
@@ -102,6 +101,11 @@ in [
         ++ [
           pkgs.perl # for openssl-sys
           pkgs.pkg-config # for openssl-sys
+        ];
+      buildInputs =
+        (m.buildInputs or [])
+        ++ [
+          pkgs.openssl
         ];
     };
   }