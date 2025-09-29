e81812fe99f6274303cf7ae933d3d3cd6d6f6f7b
GitHub Action <action@github.com>
2025-09-29 01:16:34 +0000

    ci: refresh manifests for patch PR

---
+++ manifests/forc-fmt-0.69.4.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-fmt";
+  version = "0.69.4";
+  date = "2025-09-24";
+  url = "https://github.com/fuellabs/sway";
+  rev = "a2c0531ab777d9a003d8bcc34c05880bff50c356";
+  sha256 = "sha256-AYfe6fwmqx6ZlnZ5j8G7vKzpBAc0ebBFmSLghF22/gc=";
+}
---
+++ manifests/forc-migrate-0.69.4-nightly-2025-09-26.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-migrate";
+  version = "0.69.4";
+  date = "2025-09-26";
+  url = "https://github.com/fuellabs/sway";
+  rev = "cdd8800e75d3b4a7e5d70fd8df7a563c5acc246e";
+  sha256 = "sha256-oRjtrqE5pzQG95Uu1E9tVtOnz4o3n3LHhVzdIuRbmfE=";
+}
