d24a5412b7e429a3a304b3bda5756af50bf2c874
GitHub Action <action@github.com>
2025-10-20 19:24:17 +0000

    chore: update milestones

--- milestones.nix
+++ milestones.nix
@@ -5,24 +5,24 @@
   # Commits sourced from:
   # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-testnet.toml
   testnet = {
-    forc-wallet = "9fea98d095d72c771f6a6f81f901fa4f8d5ece52";
-    fuel-core = "8e34930a63329ce60bb361d38e25f6b9a7429a05";
-    sway = "a1300bae7d7a57c96517364f6f1236927666176f";
+    forc-wallet = "6ba79bc6fb2f14acfedb4eed6197f5b56294fc38";
+    fuel-core = "e295b233078e8d236826019c4a5db13ed89a437c";
+    sway = "107b092e9d7003256321d3890a636273d0845662";
   };
 
   # Commits sourced from:
   # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-mainnet.toml
   ignition = {
-    forc-wallet = "9fea98d095d72c771f6a6f81f901fa4f8d5ece52";
-    fuel-core = "8e34930a63329ce60bb361d38e25f6b9a7429a05";
-    sway = "a1300bae7d7a57c96517364f6f1236927666176f";
+    forc-wallet = "6ba79bc6fb2f14acfedb4eed6197f5b56294fc38";
+    fuel-core = "e295b233078e8d236826019c4a5db13ed89a437c";
+    sway = "107b092e9d7003256321d3890a636273d0845662";
   };
 
   # Commits sourced from:
   # https://raw.githubusercontent.com/FuelLabs/fuelup/gh-pages/channel-fuel-mainnet.toml
   mainnet = {
-    forc-wallet = "9fea98d095d72c771f6a6f81f901fa4f8d5ece52";
-    fuel-core = "8e34930a63329ce60bb361d38e25f6b9a7429a05";
-    sway = "a1300bae7d7a57c96517364f6f1236927666176f";
+    forc-wallet = "6ba79bc6fb2f14acfedb4eed6197f5b56294fc38";
+    fuel-core = "e295b233078e8d236826019c4a5db13ed89a437c";
+    sway = "107b092e9d7003256321d3890a636273d0845662";
   };
 }
