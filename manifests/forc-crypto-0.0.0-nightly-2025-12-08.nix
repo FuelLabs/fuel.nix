ea1c64f8d1157d5fecb12ecd069a935e0bc82195
GitHub Action <action@github.com>
2025-12-08 08:43:27 +0000

    ci: refresh manifests for patch PR

---
+++ manifests/forc-crypto-0.0.0-nightly-2025-12-08.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.0.0";
+  date = "2025-12-08";
+  url = "https://github.com/fuellabs/forc";
+  rev = "d01e86b68a3e1491791733bbc61a0c96d0b4c7dd";
+  sha256 = "sha256-i2mEoi695f7LRkkhGtfOUpedHhRiLu0rggV15wlku5U=";
+}
---
+++ manifests/forc-crypto-0.71.0.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.71.0";
+  date = "2025-12-08";
+  url = "https://github.com/fuellabs/forc";
+  rev = "3f7f10bc975094e260e871e0a46383e819d5fbea";
+  sha256 = "sha256-xoKobcqnk1SAljakML0nmEQezPb0IxYyMs9EtnHwF28=";
+}
