f0d5921da3937be54819b3cbef86d453b0658bd9
GitHub Action <action@github.com>
2025-12-09 00:37:24 +0000

    manifest: update

---
+++ manifests/forc-crypto-0.0.0-nightly-2025-12-09.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.0.0";
+  date = "2025-12-09";
+  url = "https://github.com/fuellabs/forc";
+  rev = "1409bfba1321f5a01e63434b60013ea63cee897e";
+  sha256 = "sha256-Lrqi9SmPotI6UEWUteJC235ozuIkgKO07T5v9zK0iQk=";
+}
