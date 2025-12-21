39533bd879f4f319929194f7caa2d9cf61da649a
GitHub Action <action@github.com>
2025-12-21 00:29:28 +0000

    manifest: update

---
+++ manifests/forc-crypto-0.70.3.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.70.3";
+  date = "2025-12-17";
+  url = "https://github.com/fuellabs/sway";
+  rev = "aac06fb6bdc6b97c26137d0661410b576008b409";
+  sha256 = "sha256-phfm+wDMiVsx1m7Wh1/6YzTbGQ3pXSO5D8WTVAYe1TU=";
+}
---
+++ manifests/forc-debug-0.69.3.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-debug";
+  version = "0.69.3";
+  date = "2025-09-23";
+  url = "https://github.com/fuellabs/sway";
+  rev = "ec6f0ca5c7c062251713021c42fdf51223810d21";
+  sha256 = "sha256-xiNzx8cKCFAvwho6VZMIRkEdszZnd8hc0OjOXFQQqZE=";
+}
