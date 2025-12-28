580414d52509039140417468e15f53040d7dd440
GitHub Action <action@github.com>
2025-12-28 00:33:27 +0000

    manifest: update

---
+++ manifests/forc-crypto-0.70.2-nightly-2025-11-28.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.70.2";
+  date = "2025-11-28";
+  url = "https://github.com/fuellabs/sway";
+  rev = "ec51769132241ecd2e480f77d1f11ae0b56e3b2f";
+  sha256 = "sha256-LPYEaGJW/zkevhyQq/5OQ7aOTeqI2wW/uUBzNDXM8Ns=";
+}
---
+++ manifests/forc-wallet-0.16.1-nightly-2025-11-28.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.16.1";
+  date = "2025-11-28";
+  url = "https://github.com/fuellabs/forc";
+  rev = "aaf6925cb4d3cf9d2de14fcb1625269627922580";
+  sha256 = "sha256-SWPTzi7RDDBpSpYNiZEjU2/VL6oJeMuB2Mi+poX46Vw=";
+}
