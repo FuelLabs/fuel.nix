14b1322024de2195369c8b01c6df76c90f27f0a8
GitHub Action <action@github.com>
2026-01-07 02:48:54 +0000

    manifest: update

---
+++ manifests/forc-lsp-0.70.3-nightly-2026-01-07.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-lsp";
+  version = "0.70.3";
+  date = "2026-01-07";
+  url = "https://github.com/fuellabs/sway";
+  rev = "73533a4d06916113eaaf6c1e5b3f0aa86eca441c";
+  sha256 = "sha256-ESw2FTqgAQWHosusm9aLgt2bhES/JJ+nlv4nU0KX29k=";
+}
---
+++ manifests/forc-wallet-0.16.2-nightly-2026-01-07.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.16.2";
+  date = "2026-01-07";
+  url = "https://github.com/fuellabs/forc";
+  rev = "c9ee8ad0bc506bb45596ee9a0ce8e16122b3c129";
+  sha256 = "sha256-caDw/+NKZXiFF5KxVePKoxFQXtv2hivgbU/jiE65elA=";
+}
