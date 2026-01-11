84a19ba23964a98dc20b9387eda1ac914ff7fe9c
GitHub Action <action@github.com>
2026-01-11 01:00:58 +0000

    manifest: update

---
+++ manifests/forc-wallet-0.16.2-nightly-2025-12-12.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-wallet";
+  version = "0.16.2";
+  date = "2025-12-12";
+  url = "https://github.com/fuellabs/forc";
+  rev = "c10463b0cb7308aad15d6bf5abd7e047ac14a7fd";
+  sha256 = "sha256-TtVlGf6gJFrAj4R+Yrujot1rPVOreXfgnX3nSyjIqug=";
+}
