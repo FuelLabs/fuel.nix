62d37d065ff9a34b47b58c165fe424720f2c5cfa
GitHub Action <action@github.com>
2026-01-27 03:11:31 +0000

    manifest: update

---
+++ manifests/fuel-core-0.47.1-nightly-2026-01-27.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core";
+  version = "0.47.1";
+  date = "2026-01-27";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "4c44342aa758b433acb97b20ff3090d31f0d240b";
+  sha256 = "sha256-ziOnqNsQ4Jt6FMST/FjLLGodRlFPSBsYXdDM1Te4zgw=";
+}
