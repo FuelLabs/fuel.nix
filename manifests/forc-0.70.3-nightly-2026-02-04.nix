34e28f3b0608b6c3c28e1e5a57acb17c49a023b3
GitHub Action <action@github.com>
2026-02-04 06:53:24 +0000

    ci: refresh manifests for patch PR

---
+++ manifests/forc-0.70.3-nightly-2026-02-04.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc";
+  version = "0.70.3";
+  date = "2026-02-04";
+  url = "https://github.com/fuellabs/sway";
+  rev = "b12d83440750936dbefa3cb3804f8c6533c49793";
+  sha256 = "sha256-CsdSm1mc1b17D+6nZkq+MPQOYJut8FJzixQ6vk+Lwoo=";
+}
---
+++ manifests/forc-node-0.71.2-nightly-2026-01-30.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-node";
+  version = "0.71.2";
+  date = "2026-01-30";
+  url = "https://github.com/fuellabs/forc";
+  rev = "1a6613558d6d8be3a869c8d0f0040a0e1f53f977";
+  sha256 = "sha256-5NI55JAcEesnnNQ1xcAQvqb8BmyHQfHa7XsVI9NI5tc=";
+}
