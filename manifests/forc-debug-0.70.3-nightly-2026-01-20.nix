172fc0562a5f16a078b2ab7e970d336e6170bdd3
GitHub Action <action@github.com>
2026-02-19 01:37:00 +0000

    manifest: update

---
+++ manifests/forc-debug-0.70.3-nightly-2026-01-20.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-debug";
+  version = "0.70.3";
+  date = "2026-01-20";
+  url = "https://github.com/fuellabs/sway";
+  rev = "20b32cfb44ce5b7de13c2e5114ac91515f107cfa";
+  sha256 = "sha256-u3nYlHOc7QKREBR2rQCicAVmgkLs2qtTDn5fdvqL5zY=";
+}
---
+++ manifests/forc-fmt-0.70.3-nightly-2026-02-19.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-fmt";
+  version = "0.70.3";
+  date = "2026-02-19";
+  url = "https://github.com/fuellabs/sway";
+  rev = "f9de5456ea65806768cc83ddd74333fc90271711";
+  sha256 = "sha256-a3ZKYefLHlliWouXyzMB5wIQUElcv92LxSYR5t7NR1M=";
+}
