2b12cd0efb75a2ca7f41a258b323f5787c81e1d9
GitHub Action <action@github.com>
2026-02-22 00:33:10 +0000

    manifest: update

---
+++ manifests/forc-publish-0.70.3-nightly-2026-01-23.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-publish";
+  version = "0.70.3";
+  date = "2026-01-23";
+  url = "https://github.com/fuellabs/sway";
+  rev = "20b32cfb44ce5b7de13c2e5114ac91515f107cfa";
+  sha256 = "sha256-u3nYlHOc7QKREBR2rQCicAVmgkLs2qtTDn5fdvqL5zY=";
+}
