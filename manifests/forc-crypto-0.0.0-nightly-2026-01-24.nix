71166d9209e4042d464cfaba09ec17664765ea37
GitHub Action <action@github.com>
2026-02-23 00:34:22 +0000

    manifest: update

---
+++ manifests/forc-crypto-0.0.0-nightly-2026-01-24.nix
@@ -0,0 +1,8 @@
+{
+  pname = "forc-crypto";
+  version = "0.0.0";
+  date = "2026-01-24";
+  url = "https://github.com/fuellabs/forc";
+  rev = "54cf9da7d393944b585fa8628dcd36f7e110a116";
+  sha256 = "sha256-XDNMSUCQPagNV0i3bYhhGa3MqDHt73fHSi/Ypde3CS8=";
+}
---
+++ manifests/fuel-core-client-0.47.1-nightly-2026-01-24.nix
@@ -0,0 +1,8 @@
+{
+  pname = "fuel-core-client";
+  version = "0.47.1";
+  date = "2026-01-24";
+  url = "https://github.com/fuellabs/fuel-core";
+  rev = "579e1c2d78ba1094c28cab1aafe4b40eda86db31";
+  sha256 = "sha256-VCgU62x2LwCn6gUFIrfTNGB/X8QnT8SmmroT4mSMDYU=";
+}
